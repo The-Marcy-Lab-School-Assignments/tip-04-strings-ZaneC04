3f172ddae57ebcc73c11c91f143cea8df6e16cfa
ZaneC04 <zenithzsc@gmail.com>
2025-10-06 10:27:13 -0400

    solved reverseString

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ You should not use the reverse method and you should not use the reverse method 
 
 The function should return a NEW string as the output. 
 */
-function reverseString(str) {}
+const reverseString = (str) => {
+    let newStr = ""
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i]
+    }
+    return newStr
+}
 
 // Test the function
 console.log(reverseString("Hello")); // Output: "olleH"
